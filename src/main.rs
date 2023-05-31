8211371ff4a170943aaacea1c5d1e09c2a01af4a
dudoof <email@example.com>
2023-05-31 10:46:26 +0200

    w/Program Name

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    let prg_name: &String = &args[0];
     let query: &String = &args[1];
     let file_path: &String = &args[2];
 
+    println!("Program Name {}",prg_name);
     println!("Searching for {}", query);
     println!("In file {}", file_path);
 }
